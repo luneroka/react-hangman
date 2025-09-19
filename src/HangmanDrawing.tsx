df23d07a35ecbe83bffdd1369b2c7b35843ebb4e
luneroka <luneroka@gmail.com>
2025-09-19 16:40:57 +0200

    Create HangmanDrawing props

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -73,14 +73,15 @@ const LEFT_LEG = (
     }} />
 )
 
-export function HangmanDrawing() {
+const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG]
+
+type HangmanDrawingProps = {
+    numberOfGuesses: number
+}
+
+export function HangmanDrawing({ numberOfGuesses }: HangmanDrawingProps) {
     return <div style={{ position: "relative" }}>
-        {HEAD}
-        {BODY}
-        {RIGHT_ARM}
-        {LEFT_ARM}
-        {RIGHT_LEG}
-        {LEFT_LEG}
+        {BODY_PARTS.slice(0, numberOfGuesses)}
         <div style={{ height: "50px", width: "10px", background: "black", top: "0", right: "0", position: "absolute" }} />
         <div style={{ height: "10px", width: "200px", background: "black", marginLeft: "120px" }} />
         <div style={{ height: "400px", width: "10px", background: "black", marginLeft: "120px" }} />
