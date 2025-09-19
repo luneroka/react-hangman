7d4e7d543b8b47bf971919de36948606d9c7647d
luneroka <luneroka@gmail.com>
2025-09-19 16:41:10 +0200

    Create HangmanWord props

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,9 +1,12 @@
-export function HangmanWord() {
-    const word = "test";
-    const guessedLetters = ["t", "e"];
+type HangmanWordProps = {
+    guessedLetters: string[]
+    wordToGuess: string
+}
+
+export function HangmanWord({ guessedLetters, wordToGuess}: HangmanWordProps) {
 
     return <div style={{ display: "flex", gap: ".25em", fontSize: "6rem", fontWeight: "bold", textTransform: "uppercase", fontFamily: "monospace" }}>
-        {word.split("").map((letter, index) => (
+        {wordToGuess.split("").map((letter, index) => (
             <span style={{ borderBottom: ".1em solid black"}} key={index}>
                 <span 
                     style={{ 
